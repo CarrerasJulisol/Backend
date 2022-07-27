fe3aa0b79f2c59e0968a8fea5c7137ade7152512
JuliSol <julisolcarreras@gmail.com>
2022-07-27 15:11:53 -0300

    Motor de plantilla Handlebars listo

---
+++ MotoresDePlantilla/Handlebars/src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import __dirname from './utils.js';
+import productosRouter from './routes/products.router.js';
+import viewsRouter from './routes/views.router.js'
+const app = express();
+const PORT = 8080;
+const server = app.listen(PORT,()=>console.log(`Listening on ${server.address().port}`));
+
+app.engine('handlebars',handlebars.engine());
+app.set('views',__dirname+'/views');
+app.set('view engine','handlebars')
+
+app.use(express.json())
+app.use(express.static(__dirname+'/public'))
+app.use('/',viewsRouter)
+app.use('/api/productos',productosRouter)
---
+++ MotoresDePlantilla/Handlebars/src/routes/products.router.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { uploader } from '../utils.js'
+import __dirname from "../utils.js";
+import Container from "../context/container.js";
+
+const router = Router()
+const File = new Container()
+
+router.post('/',uploader.single('image'),async (req, res)=> {
+    const { name, price } = req.body
+    console.log(req.file)
+    console.log(req.body)
+    console.log(name)
+    console.log(price)
+    let product = {
+        name,
+        price,
+        image: req.file.filename,
+    }
+    await File.save(product)
+    res.send({status:"success",message:"Producto creado."})
+})
+
+
+export default router
