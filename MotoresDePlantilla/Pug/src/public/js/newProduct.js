3849761db62a86dd8c5062d28bfb13ec807c35ce
JuliSol <julisolcarreras@gmail.com>
2022-07-27 21:44:31 -0300

    agregado motor de plantilla Pug

---
+++ MotoresDePlantilla/Pug/src/public/js/newProduct.js
@@ -0,0 +1,11 @@
+const Form = document.getElementById('productForm');
+
+Form.addEventListener('submit',evt=>{
+    evt.preventDefault();
+    const formData = new FormData(Form);
+    console.log(formData)
+    fetch('/api/productos',{
+        method:"POST",
+        body:formData,
+    }).then(result=>result.json()).then(json=>console.log(json));
+})
